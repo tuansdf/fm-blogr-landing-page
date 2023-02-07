19d8c9a4cec2d7fc6f84378ae6e61f6681c4356c
tuansdf <103062682+tuansdf@users.noreply.github.com>
2023-02-07 21:15:04 +0700

    setup tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,14 @@
-import './globals.css'
+import "/src/globals.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+interface IProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: IProps) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
